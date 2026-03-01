f5ef4a756103987c00db25c617306dc8f3824612
Kevin George <kevingeorge2409@gmail.com>
2026-03-01 19:37:39 +0530

    made all the hero section changes

--- app/(marketing)/_components/hero-section.tsx
+++ app/(marketing)/_components/hero-section.tsx
@@ -6,6 +6,15 @@ import Image from 'next/image'
 import { TextEffect } from '@/components/ui/text-effect'
 import { AnimatedGroup } from '@/components/ui/animated-group'
 import { HeroHeader } from './header'
+import BackgroundImage from '@/public/night-background.webp'
+import HeroDark from '@/public/hero-dark.webp'
+import HeroLight from '@/public/hero-light.webp'
+import Arkjet from '@/public/companies/arkjet.webp'
+import motion from '@/public/companies/motion.webp'
+import orpc from '@/public/companies/orpc.webp'
+import neon from '@/public/companies/neon.webp'
+import vercel from '@/public/companies/vercel.webp'
+import prisma from '@/public/companies/prisma.webp'
 
 const transitionVariants = {
     item: {
@@ -19,7 +28,7 @@ const transitionVariants = {
             filter: 'blur(0px)',
             y: 0,
             transition: {
-                type: 'spring',
+                type: 'spring' as const,
                 bounce: 0.3,
                 duration: 1.5,
             },
@@ -68,7 +77,7 @@ export default function HeroSection() {
                             }}
                             className="mask-b-from-35% mask-b-to-90% absolute inset-0 top-56 -z-20 lg:top-32">
                             <Image
-                                src="https://ik.imagekit.io/lrigu76hy/tailark/night-background.jpg?updatedAt=1745733451120"
+                                src={BackgroundImage}
                                 alt="background"
                                 className="hidden size-full dark:block"
                                 width="3276"
@@ -87,7 +96,7 @@ export default function HeroSection() {
                                     <Link
                                         href="#link"
                                         className="hover:bg-background dark:hover:border-t-border bg-muted group mx-auto flex w-fit items-center gap-4 rounded-full border p-1 pl-4 shadow-md shadow-zinc-950/5 transition-colors duration-300 dark:border-t-white/5 dark:shadow-zinc-950">
-                                        <span className="text-foreground text-sm">Introducing Support for AI Models</span>
+                                        <span className="text-foreground text-sm">Introducing New AI-Ready Features</span>
                                         <span className="dark:border-background block h-4 w-0.5 border-l bg-white dark:bg-zinc-700"></span>
 
                                         <div className="bg-background group-hover:bg-muted size-6 overflow-hidden rounded-full duration-500">
@@ -108,7 +117,7 @@ export default function HeroSection() {
                                     speedSegment={0.3}
                                     as="h1"
                                     className="mx-auto mt-8 max-w-4xl text-balance text-5xl max-md:font-semibold md:text-7xl lg:mt-16 xl:text-[5.25rem]">
-                                    Modern Solutions for Customer Engagement
+                                    The AI-Ready Home For Team Communication
                                 </TextEffect>
                                 <TextEffect
                                     per="line"
@@ -117,7 +126,7 @@ export default function HeroSection() {
                                     delay={0.5}
                                     as="p"
                                     className="mx-auto mt-8 max-w-2xl text-balance text-lg">
-                                    Highly customizable components for building modern websites and applications that look and feel the way you mean it.
+                                    Looped organizes conversations into channels with threads, is realtime, and uses AI to keep teams in sync.
                                 </TextEffect>
 
                                 <AnimatedGroup
@@ -132,7 +141,7 @@ export default function HeroSection() {
                                         },
                                         ...transitionVariants,
                                     }}
-                                    className="mt-12 flex flex-col items-center justify-center gap-2 md:flex-row">
+                                    className="mt-12 flex flex-col items-center justify-center gap-5 md:flex-row">
                                     <div
                                         key={1}
                                         className="bg-foreground/10 rounded-[calc(var(--radius-xl)+0.125rem)] border p-0.5">
@@ -141,7 +150,7 @@ export default function HeroSection() {
                                             size="lg"
                                             className="rounded-xl px-5 text-base">
                                             <Link href="#link">
-                                                <span className="text-nowrap">Start Building</span>
+                                                <span className="text-nowrap">Get Started</span>
                                             </Link>
                                         </Button>
                                     </div>
@@ -149,7 +158,7 @@ export default function HeroSection() {
                                         key={2}
                                         asChild
                                         size="lg"
-                                        variant="ghost"
+                                        variant="secondary"
                                         className="h-10.5 rounded-xl px-5">
                                         <Link href="#link">
                                             <span className="text-nowrap">Request a demo</span>
@@ -174,15 +183,15 @@ export default function HeroSection() {
                             <div className="mask-b-from-55% relative -mr-56 mt-8 overflow-hidden px-2 sm:mr-0 sm:mt-12 md:mt-20">
                                 <div className="inset-shadow-2xs ring-background dark:inset-shadow-white/20 bg-background relative mx-auto max-w-6xl overflow-hidden rounded-2xl border p-4 shadow-lg shadow-zinc-950/15 ring-1">
                                     <Image
-                                        className="bg-background aspect-15/8 relative hidden rounded-2xl dark:block"
-                                        src="/mail2.png"
+                                        className="bg-background aspect-15/8 relative hidden rounded-2xl dark:block object-contain object-top"
+                                        src={HeroDark}
                                         alt="app screen"
                                         width="2700"
                                         height="1440"
                                     />
                                     <Image
-                                        className="z-2 border-border/25 aspect-15/8 relative rounded-2xl border dark:hidden"
-                                        src="/mail2-light.png"
+                                        className="z-2 border-border/25 aspect-15/8 relative rounded-2xl border dark:hidden object-contain object-top"
+                                        src={HeroLight}
                                         alt="app screen"
                                         width="2700"
                                         height="1440"
@@ -205,20 +214,18 @@ export default function HeroSection() {
                         </div>
                         <div className="group-hover:blur-xs mx-auto mt-12 grid max-w-2xl grid-cols-4 gap-x-12 gap-y-8 transition-all duration-500 group-hover:opacity-50 sm:gap-x-16 sm:gap-y-14">
                             <div className="flex">
-                                <img
-                                    className="mx-auto h-5 w-fit dark:invert"
-                                    src="https://html.tailus.io/blocks/customers/nvidia.svg"
-                                    alt="Nvidia Logo"
-                                    height="20"
-                                    width="auto"
+                                <Image
+                                    className="mx-auto h-7 object-contain invert dark:invert-0"
+                                    src={Arkjet}
+                                    alt="Arkjet Logo"
                                 />
                             </div>
 
                             <div className="flex">
                                 <img
                                     className="mx-auto h-4 w-fit dark:invert"
-                                    src="https://html.tailus.io/blocks/customers/column.svg"
-                                    alt="Column Logo"
+                                    src={motion.src}
+                                    alt="Motion Logo"
                                     height="16"
                                     width="auto"
                                 />
@@ -226,8 +233,8 @@ export default function HeroSection() {
                             <div className="flex">
                                 <img
                                     className="mx-auto h-4 w-fit dark:invert"
-                                    src="https://html.tailus.io/blocks/customers/github.svg"
-                                    alt="GitHub Logo"
+                                    src={orpc.src}
+                                    alt="ORPC Logo"
                                     height="16"
                                     width="auto"
                                 />
@@ -235,8 +242,8 @@ export default function HeroSection() {
                             <div className="flex">
                                 <img
                                     className="mx-auto h-5 w-fit dark:invert"
-                                    src="https://html.tailus.io/blocks/customers/nike.svg"
-                                    alt="Nike Logo"
+                                    src={vercel.src}
+                                    alt="Vercel Logo"
                                     height="20"
                                     width="auto"
                                 />
@@ -244,8 +251,8 @@ export default function HeroSection() {
                             <div className="flex">
                                 <img
                                     className="mx-auto h-5 w-fit dark:invert"
-                                    src="https://html.tailus.io/blocks/customers/lemonsqueezy.svg"
-                                    alt="Lemon Squeezy Logo"
+                                    src={prisma.src}
+                                    alt="Prisma Logo"
                                     height="20"
                                     width="auto"
                                 />
